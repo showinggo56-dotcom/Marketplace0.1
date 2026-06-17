aad259987d2621318153eb4fc805f0608be78a1f
showinggo56-dotcom <showinggo56@gmail.com>
2026-06-17 15:07:04 +0530

    Add Google OAuth provider to main app entry

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,21 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { CartProvider } from '@/contexts/CartContext';
 import { Toaster } from '@/components/ui/sonner';
 import './index.css';
 import App from './App.tsx';
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-          <Toaster position="top-right" richColors />
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <BrowserRouter>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+            <Toaster position="top-right" richColors />
+          </CartProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 );
